96a0e8fb1e43a0f1d48d22a548d362ef3844a182
greatreyhan <greatreyhan@students.undip.ac.id>
2024-05-29 12:07:19 +0700

    update env

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -19,7 +19,7 @@ const Card: React.FC<CardProps> = ({ city, img, quota, tanggal, data, place, isD
             <p className='text-center font-semibold text-md mt-4'>{tanggal}</p>
             <p className='text-center text-sm mt-2'>{place}</p>
             {isDisable ? 
-            <Link href="#" className="bg-slate-700 rounded-none border-none px-8 py-2 text-sm text-center mt-4 cursor-none hover:shadow-none">Telah Berlangsung</Link>
+            <Link href="#" className="bg-slate-700 rounded-none border-none px-8 py-2 text-sm text-center mt-4 cursor-none hover:shadow-none ">Telah Berlangsung</Link>
             :
             <Link href="/register-offline" className="bg-red-700 rounded-none border-none px-8 py-2 text-sm text-center mt-4 shadow-[0_10px_50px_rgba(185,_28,_28,_0.5)] hover:bg-primary/80 hover:shadow-none">Daftar di Sini</Link>
             }            
