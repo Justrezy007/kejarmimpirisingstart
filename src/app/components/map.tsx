a0e23ec0e67a2aadcfcc999c87390d35e357cf13
greatreyhan <greatreyhan@students.undip.ac.id>
2024-06-01 09:18:27 +0700

    update disable jakarta

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -37,7 +37,7 @@ const Map = () => {
       <div className='w-full h-full flex flex-wrap gap-5 justify-around items-center absolute top-0 left-0 px-4 md:px-32'>
         
         <Card city={"Yogyakarta"} img={'/jogja.jpg'} quota={75} tanggal={"Sabtu, 18 Mei 2024"} data={data[2]} place={'Defreshco Coffee & Bistro'} isDisable={true} />
-        <Card city={"Jakarta"} img={'/jakarta.jpg'} quota={150} tanggal={"Sabtu, 1 Juni 2024"} data={data[0]} place={'CIMB Niaga Icon Pondok Indah'} isDisable={false}/>
+        <Card city={"Jakarta"} img={'/jakarta.jpg'} quota={150} tanggal={"Sabtu, 1 Juni 2024"} data={data[0]} place={'CIMB Niaga Icon Pondok Indah'} isDisable={true}/>
         <Card city={"Manado"} img={'/manado.jpeg'} quota={75} tanggal={"Sabtu, 8 Juni 2024"} data={data[1]} place={'Bumi Beringin Drinks & Eatery'} isDisable={false} />
 
       </div>
