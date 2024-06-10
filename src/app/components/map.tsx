93121ff1cef4b789452aa9cc76967f128df0391c
greatreyhan <greatreyhan@students.undip.ac.id>
2024-06-10 19:48:15 +0700

    update timeline

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -38,7 +38,7 @@ const Map = () => {
         
         <Card city={"Yogyakarta"} img={'/jogja.jpg'} quota={75} tanggal={"Sabtu, 18 Mei 2024"} data={data[2]} place={'Defreshco Coffee & Bistro'} isDisable={true} />
         <Card city={"Jakarta"} img={'/jakarta.jpg'} quota={150} tanggal={"Sabtu, 1 Juni 2024"} data={data[0]} place={'CIMB Niaga Icon Pondok Indah'} isDisable={true}/>
-        <Card city={"Manado"} img={'/manado.jpeg'} quota={75} tanggal={"Sabtu, 8 Juni 2024"} data={data[1]} place={'Bumi Beringin Drinks & Eatery'} isDisable={false} />
+        <Card city={"Manado"} img={'/manado.jpeg'} quota={75} tanggal={"Sabtu, 8 Juni 2024"} data={data[1]} place={'Bumi Beringin Drinks & Eatery'} isDisable={true} />
 
       </div>
       <Image className='h-72' src={"/Map.svg"} alt='map' width={780} height={320} />
